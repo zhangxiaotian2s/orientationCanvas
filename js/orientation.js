edf5a074420491de0dd6a4fc388185ce10491115
zhangxiaotian2s <506834853@qq.com>
2016-06-03 11:26:17 +0800

    xxxxxx

--- js/orientation.js
+++ js/orientation.js
@@ -1,29 +1,22 @@
 function imgArrShow(imgarr) {
 	this.width = document.body.clientWidth > 640 ? 640 : document.body.clientWidth;
 	this.height = this.width;
-	this.box = document.getElementById('detail-canvasbox')
 	this.canvas = document.getElementById('canvas');
-	this.canvasbk = document.getElementById('canvasbk');
-	this.ctx = this.canvas.getContext('2d');
-	this.ctxbk=this.canvasbk.getContext('2d');
+	this.ctx = self.canvas.getContext('2d');
 	this.imgarr = imgarr;
 	this.imglength = this.imgarr.length;
 	this.loadimgsrc = 'http://7xl619.com1.z0.glb.clouddn.com/load.gif?10010'
 	this.loadingbox = document.querySelector('#loadingbox')
 	this.boolmobile = navigator.userAgent.match(/(iPhone|iPod|Android|ios)/i)
-	this.showindex = null
-	this.timer = null;
+	this.showindex = 0
 };
 var imgArrShowPropty = imgArrShow.prototype
 imgArrShowPropty.init = function() {
 	var self = this;
 	self.canvas.setAttribute('width', self.width + 'px');
 	self.canvas.setAttribute('height', self.height + 'px');
-	self.canvasbk.setAttribute('width', self.width + 'px');
-	self.canvasbk.setAttribute('height', self.height + 'px');
 	self.loadWait(true)
 	self.loadAllImg();
-	self.drawArrImg(0)
 	if (Boolean(self.boolmobile)) {
 		self.mobileDirection()
 	} else {
@@ -58,7 +51,7 @@ imgArrShowPropty.mobileDirection = function() {
 //pc端处理
 imgArrShowPropty.pcMouseMove = function() {
 	var self = this;
-	self.box.addEventListener('mousemove', function(e) {
+	self.canvas.addEventListener('mousemove', function(e) {
 		console.log(this.offsetLeft)
 		var _position_x = e.pageX - this.parentElement.offsetLeft;
 		_position_x < 0 ? 0 : _position_x;
@@ -81,8 +74,7 @@ imgArrShowPropty.loadWait = function(bool) {
 		//		self.ctx.clearRect(0, 0, self.width, self.height)
 		//		self.drawArrImg(Math.floor(self.imgarr.length) / 2)
 		self.loadingbox.style.display = 'none'
-			//		self.drawArrImg(Math.floor(self.imgarr.length) / 2)
-		self.drawArrImg(0)
+		self.drawArrImg(Math.floor(self.imgarr.length) / 2)
 	}
 };
 //drawimg 图片进入画布
@@ -91,20 +83,15 @@ imgArrShowPropty.drawArrImg = function(index) {
 	if (index < 0) {
 		index = 0;
 	}
-	var _imgbk = new Image();
-	   _imgbk = self.imgarr[self.showindex || index]
 	if (self.showindex != index) {
 		self.showindex = index;
-		var _img = new Image();
+		var _img = new Image;
 		_img.src = self.imgarr[index];
 		_img.onload = function() {
-			clearInterval(self.timer)
+			self.ctx.clearRect(0, 0, self.width, self.height)
 			var _img_w = self.width;
 			var _img_h = Math.floor((self.width / _img.width) * _img.height);
-			self.ctx.clearRect(0, 0, self.width, self.height);
 			self.ctx.drawImage(_img, 0, 0, _img_w, _img_h);
-			self.ctxbk.drawImage(_img, 0, 0, _img_w, _img_h);
-			
 		}
 	}
 };
